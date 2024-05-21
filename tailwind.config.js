d86489bbe152eb54b6b2c3b98acacc0ba4b8f496
Sreyka Kosal <108143296+KosalSreyka@users.noreply.github.com>
2024-05-21 18:56:29 +0700

    forget password done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // Custom colors...
+        'color-text-black': '#475568',
+        'color-text-grey':'#949494',
+        'color-text-white':'#F9FAFB',
+        'primary-color':'#1A42BC',
+        'neutral-color':'#F0F0F0',
+        'secodary-color':'#B030FF',
+        'color-finish':'#00D33B',
+        'color-pending':'#D20303',
+        'color-process':'#F8C408',
+      }
     },
   },
   plugins: [],
