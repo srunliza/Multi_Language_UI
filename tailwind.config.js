05aa2dd4b1f5bc8331b78c44fbb5fdb184207b21
Kroem Vandy <kroem123vandy@gmail.com>
2024-05-26 10:17:45 +0700

    done auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,8 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          'custom-pattern': 'repeating-linear-gradient(45deg, #444cf6 0, #443cf6 0.1px, #e5e5f7 0, #e5e4f7 49%)',
+        "custom-pattern": 'repeating-linear-gradient(45deg, #444cf6 0, #443cf6 0.1px, #e5e5f7 0, #e5e4f7 49%)',
+
       },
 
       colors: {
