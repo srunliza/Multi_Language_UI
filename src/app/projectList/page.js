5240d370c32797a0be0fa261b7cf017676037fc5
longsreyly <longsreyly31@gmail.com>
2024-05-27 08:30:02 +0700

    update setting

--- src/app/projectList/page.js
+++ src/app/projectList/page.js
@@ -54,10 +54,10 @@ function ProjectPage() {
             </div>
 
             {/* <!-- Main content --> */}
-            <div class="flex flex-col flex-1 overflow-y-auto">
-                <div class="flex items-center justify-between p-3 bg-white border-b border-gray-200">
-                    <div class="flex items-center px-4">
-                        <button class="text-gray-500 focus:outline-none focus:text-gray-700">
+            <div className="flex flex-col flex-1 overflow-y-auto ">
+                <div className="flex items-center justify-between p-3 bg-white border-b border-gray-200">
+                    <div className="flex items-center px-4">
+                        <button className="text-gray-500 focus:outline-none focus:text-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                 stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -66,7 +66,7 @@ function ProjectPage() {
                         </button>
                         <input class="mx-4 w-full  border-blue-200 border-2 rounded-md px-4 py-2" type="text" placeholder="Search" />
                     </div>
-                    <div class="flex items-center pr-4">                     
+                    <div className="flex items-center pr-4">                     
                     <div className="flex items-center pr-4 gap-5">
                             <button className="btn btn-ghost btn-circle">
                                 <img src="./notification.svg" alt="notification" />
@@ -78,7 +78,7 @@ function ProjectPage() {
                         </div>
                     </div>
                 </div>
-                <div class="p-4">
+                <div className="p-4">
                     <ProjectListComponent/>
                 </div>
             </div>
--- src/app/setting/page.js
+++ src/app/setting/page.js
@@ -54,19 +54,19 @@ const SettingPage = () => {
             </div>
 
             {/* <!-- Main content --> */}
-            <div class="flex flex-col flex-1 overflow-y-auto">
-                <div class="flex items-center justify-between h-16 bg-white border-b border-gray-200">
-                    <div class="flex items-center px-4">
-                        <button class="text-gray-500 focus:outline-none focus:text-gray-700">
+            <div className="flex flex-col flex-1 overflow-y-auto">
+                <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
+                    <div className="flex items-center px-4">
+                        <button className="text-gray-500 focus:outline-none focus:text-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                 stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
-                        <input class="mx-4 w-full  border-blue-200 border-2 rounded-md px-4 py-2" type="text" placeholder="Search" />
+                        <input className="mx-4 w-full  border-blue-200 border-2 rounded-md px-4 py-2" type="text" placeholder="Search" />
                     </div>
-                    <div class="flex items-center pr-4">
+                    <div className="flex items-center pr-4">
 
                        
                     <div className="flex items-center pr-4 gap-5">
