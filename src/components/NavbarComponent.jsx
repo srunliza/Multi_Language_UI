bc7f840936aa8a8e325c8df8247d50450241a435
Kroem Vandy <kroem123vandy@gmail.com>
2024-05-27 07:52:25 +0700

    done employee dashboard 60%

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -5,7 +5,7 @@ export default function NavbarComponent() {
 
     <main>
         <div class=" bg-gray-100">
-        {/* <SidebarComponenet /> */}
+        {/* <SidebarComponent /> */}
             <div class="flex flex-col flex-1 overflow-y-auto">
                 <div class="flex items-center justify-between h-16 bg-white border-b border-gray-200">
                     <div class="flex items-center px-4 ">
