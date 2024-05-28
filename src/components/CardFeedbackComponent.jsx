bb13e5d5105e6e0eb1df4e0d509f7f6177cc1c17
longsreyly <longsreyly31@gmail.com>
2024-05-28 09:21:34 +0700

    add logo

--- src/components/CardFeedbackComponent.jsx
+++ src/components/CardFeedbackComponent.jsx
@@ -1,11 +1,9 @@
 const CardFeedbackComponent = () => {
     return (
-            <div className="w-[486px] h-[650px] shadow-md  bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-[486px] h-[650px] shadow-md  bg-white border border-gray-200 rounded-lg  sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 <div className="flex items-center justify-between mb-4">
-                    <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
-                    <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                        View all
-                    </a>
+                    <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Feedback</h5>
+                   
                 </div>
                 <div className="flow-root">
                     <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
