aa5c8f196e5f432ad5c8105e1b47f18f00c3c435
Kroem Vandy <kroem123vandy@gmail.com>
2024-06-02 00:26:28 +0700

    modified preview json page

--- src/app/preview-string-file/page.jsx
+++ src/app/preview-string-file/page.jsx
@@ -1,28 +1,129 @@
-import NavbarComponent from "@/components/NavbarComponent";
-import PreviewStringFileComponent from "@/components/PreviewStringFileComponent";
-import SidebarComponent from "@/components/SidebarComponent";
+import FeedbackComponent from "./FeedbackComponent";
 
 const PreviewStringFilePage = () => {
-    return ( 
-        <main>
-            <div className="grid grid-cols-12 bg-slate-400">
-                {/* sidebar */}
-                <div className="col-span-2 bg-black-200">
-                    <SidebarComponent/>
-                </div>
 
-                <div className="col-span-10 bg-gray-50">
-                    {/* navbar */}
-                    <div className="">
-                    <NavbarComponent/>
-                    </div>  
-                    
-                    {/* preview string component */}
-                    <div>
-                         <PreviewStringFileComponent/>
+    const previewStringData = [
+       
+        {
+            'id': 1,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 2,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 3,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 4,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 5,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 6,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 7,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 8,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 9,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 10,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 11,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 12,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 13,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 14,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 15,
+            'key' : "Hello",
+            'value' : "Hello"
+        },
+        {
+            'id': 16,
+            'key' : "Hello",
+            'value' : "Hello"
+        }
+    ]
+
+
+    return ( 
+        <main className="bg-white p-8">
+            <div className="grid grid-cols-12 gap-8">
+                <div className="col-span-8">
+                    <div className="bg-white shadow-md p-6 rounded-md min-h-[560px]">
+
+                        {/* title */}
+                        <h1 className="text-gray-800 text-xl font-semibold mb-3">Preview Page</h1>
+                        <hr />
+
+                       
+                        <div className='overflow-auto  max-h-[455px] mb-4 no-scrollbar'>
+                        {/* data map as string data */}
+                           {previewStringData.map((stringData, index) => (
+                             <div key={index} className="text-black pl-8 mt-3">
+                                <p>"{stringData.key}" : "{stringData.value}",</p>
+                             </div>
+                           ))}
+                           
+                        </div>
+                    </div>
+
+                    {/* button go back and download */}
+                    <div className="flex gap-4 mt-4 justify-end">
+                        <a href="" className="text-white bg-[#7c40ff] rounded-lg text-sm py-2 px-3">Go Back</a>
+                        <a href="" className="text-white bg-[#7c40ff] rounded-lg text-sm py-2 px-3">Download</a>
                     </div>
                 </div>
-            </div>
+
+
+                {/* feedback  */}
+                <div className="col-span-4">
+                    <FeedbackComponent/>
+                </div>
+
+            </div>          
+      
         </main>
      );
 }
