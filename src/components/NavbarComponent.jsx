492374687fa1ae82fdc51dfdb23b12065ff44dd1
Panha <chhelpanha2@gmail.com>
2024-06-03 13:57:22 +0700

    modify nav and sidebar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 
 const NavbarComponent = () => {
   return (
@@ -10,7 +10,7 @@ const NavbarComponent = () => {
             placeholder="Search project"
             class="border border-gray-300 rounded-lg py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-600"
           />
-          <svg
+          {/* <svg
             class="w-5 h-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2"
             fill="none"
             stroke="currentColor"
@@ -23,14 +23,14 @@ const NavbarComponent = () => {
               stroke-width="2"
               d="M21 21l-4.35-4.35M18 10.5A7.5 7.5 0 1110.5 3 7.5 7.5 0 0118 10.5z"
             ></path>
-          </svg>
+          </svg> */}
         </div>
       </div>
 
       <div class="flex items-center space-x-4">
         <div class="relative">
           <button class="focus:outline-none">
-            <svg
+            {/* <svg
               class="w-6 h-6 text-blue-600"
               fill="none"
               stroke="currentColor"
@@ -43,7 +43,8 @@ const NavbarComponent = () => {
                 stroke-width="2"
                 d="M15 17h5l-1.405-1.405a2.032 2.032 0 00-.364-.301l-.231-.133c-.522-.3-1.167-.549-1.952-.549a4.75 4.75 0 01-4.75-4.75c0-.785.248-1.43.549-1.952l.133-.231c.088-.152.17-.273.301-.364L15 2.752V7h-3.992a6.743 6.743 0 00-.833.183c-1.26.323-2.43 1.217-3.208 2.413A8.482 8.482 0 006 14.5c0 1.254.303 2.441.834 3.5H9v3H3v-3h1.166A7.5 7.5 0 005 14.5c0-1.286-.316-2.508-.835-3.5h2.383a6.749 6.749 0 00.833-.183H9V3h6v2.752L12 7.752a3.75 3.75 0 013 3.746v.252c0 .973-.296 1.827-.75 2.451a3.718 3.718 0 01-2.45.75v.252A6.75 6.75 0 0015 17z"
               ></path>
-            </svg>
+            </svg> */}
+            <NotificationsOutlinedIcon  />
           </button>
 
           <span class="absolute top-0 right-0 inline-block w-2.5 h-2.5 bg-red-500 rounded-full"></span>
--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -1,51 +1,88 @@
 import Link from "next/link";
+import IMAGES from "../../public/assets/images";
+import logo from "../../public/assets/images/logo.png"
+import DashboardCustomizeOutlinedIcon from '@mui/icons-material/DashboardCustomizeOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import AddToPhotosOutlinedIcon from '@mui/icons-material/AddToPhotosOutlined';
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+
+
 const SidebarComponent = () => {
-    return (
-        <div className="flex h-screen bg-gray-100">
-            {/* <!-- sidebar --> */}
-            <div className="hidden md:flex flex-col max-w-[200px] bg-blue-800 ">
-                <div className="flex items-center justify-center h-30 bg-blue-800 ">
-                    <img className=" bg-blue-800" src="./image/logo.png" alt="multi logo" />
-                </div>
-                <div className="flex flex-col flex-1 ">
-                    <nav className="flex-1 ">
-                        <div>
-                            <button type="button" className="ml-1 gap-1 px-6 py-3.5 text-base font-medium text-blue-800 inline-flex items-center bg-white rounded-t-lg rounded-br-lg  text-center  ">
-                              <img src="./icon/newproject.svg" alt="" />
-                                New Project
-                            </button>
-                            <Link href="#" className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl  flex items-center px-6 py-4 mt-2 text-gray-100  gap-2  font-light-[16px]">
-                            <img src="./icon/dashboard.svg" alt="" />
-                                <p> Dashboard</p>
-                            </Link>
-                            <Link href="#" className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2 font-light-[16px]">
-                            <img src="./icon/profile_icon.svg" alt="" />
-                                <p> Profile</p>
-
-                            </Link>
-                            <Link href="/calender" className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]">
-                            <img src="./icon/schedule_icon.svg" alt="" />
-                                <p>Calender</p>
-                            </Link>
-                            <Link href="#" className="hover:bg-gray-400 hover:bg-opacity-25  rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]">
-                            <img src="./icon/project.svg" alt="" />
-                                <p> Project</p>
-                            </Link>
-                        </div>
-
-                    </nav>
-                    <div className="mb-[50px] px-2">
-                        <Link href="/setting" className=" hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]">
-                        <img src="./icon/setting.svg" alt="" />
-                            <p>Setting</p>
-                        </Link>
-                        <Link href="#" className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]">
-                        <img src="./icon/logout.svg" alt="" />
-                            <p> Logout</p>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  console.log("logo: ", logo)
+  return (
+    <div className="hidden md:flex h-full md:items-center gap-4 flex-col bg-blue-800 ">
+      <div className="flex items-center justify-center h-30 bg-blue-800 ">
+        <img className=" bg-blue-800" src={logo} alt="multi logo" />
+      </div>
+      <div className="flex flex-col flex-1 ">
+        <nav className="flex-1">
+          <div>
+            <button
+              type="button"
+              className="ml-1 mt-1 gap-1 px-6 py-3.5 text-base font-medium text-blue-800 inline-flex items-center bg-white rounded-t-lg rounded-br-lg text-center  "
+            >
+              <img src="./icon/newproject.svg" alt="" />
+              <AddToPhotosOutlinedIcon />
+              New Project
+            </button>
+            <Link
+              href="#"
+              className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl  flex items-center px-6 py-4 mt-2 text-gray-100  gap-2  font-light-[16px]"
+            >
+              <img src="./icon/dashboard.svg" alt="" />
+              <DashboardCustomizeOutlinedIcon />
+              <p> Dashboard</p>
+            </Link>
+            <Link
+              href="#"
+              className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2 font-light-[16px]"
+            >
+              <img src="./icon/profile_icon.svg" alt="" />
+              <AccountBoxOutlinedIcon />
+              <p> Profile</p>
+            </Link>
+            <Link
+              href="/calender"
+              className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]"
+            >
+              <img src="./icon/schedule_icon.svg" alt="" />
+              <CalendarMonthOutlinedIcon />
+              <p>Calender</p>
+            </Link>
+            <Link
+              href="#"
+              className="hover:bg-gray-400 hover:bg-opacity-25  rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]"
+            >
+              <img src="./icon/project.svg" alt="" />
+              <PieChartOutlinedIcon />
+              <p>Project</p>
+            </Link>
+          </div>
+        </nav>
+        <div className="mb-[50px] px-2">
+          <Link
+            href="/setting"
+            className=" hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]"
+          >
+            <img src="./icon/setting.svg" alt="" />
+            <SettingsOutlinedIcon />
+            <p>Setting</p>
+          </Link>
+          <Link
+            href="#"
+            className="hover:bg-gray-400 hover:bg-opacity-25 rounded-2xl flex items-center px-6 py-4 mt-2 text-gray-100 gap-2  font-light-[16px]"
+          >
+            <img src="./icon/logout.svg" alt="" />
+            <ExitToAppOutlinedIcon />
+            <p> Logout</p>
+          </Link>
         </div>
+      </div>
+    </div>
+  );
+};
 
 export default SidebarComponent;
