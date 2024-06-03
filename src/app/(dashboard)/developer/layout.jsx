e06f540d2fc88cc6db94749dc8adb26ffd76911d
Panha <chhelpanha2@gmail.com>
2024-06-03 14:52:34 +0700

    modify sidebar's navigation

--- src/app/(dashboard)/developer/layout.jsx
+++ src/app/(dashboard)/developer/layout.jsx
@@ -1,18 +1,19 @@
 import NavbarComponent from "@/components/NavbarComponent";
-import SidebarComponent from "@/components/SidebarComponent";
+import Sidebar from "./_components/Sidebar";
 
 export default function DeveloperDashboardLayout({ children }) {
   return (
-    <div className="flex h-screen">
-      {/* <!-- Sidebar --> */}
-      {/* <div className="w-64 h-full bg-red-400 hidden md:block"> */}
-        <SidebarComponent />
-        {/* <!-- Sidebar content here --> */}
-      {/* </div> */}
-      <div className="flex flex-col flex-grow ">
-        {/* <h1>Navbar</h1> */}
+    <div className="grid grid-cols-12 w-full h-screen grid-rows-12">
+      <div className="bg-red-500 col-span-2 row-span-12">
+        <Sidebar />
+      </div>
+      <div className="bg-slate-500 col-span-10 row-span-1">
         <NavbarComponent />
-        <div className="flex-grow p-6 bg-white">{children}</div>
+      </div>
+      <div className="   col-span-10 row-span-12 rounded-md w-full h-full p-4">
+        <div className="bg-yellow-300 shadow-2xl w-full h-full rounded-md">
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/app/(dashboard)/translator/layout.jsx
+++ src/app/(dashboard)/translator/layout.jsx
@@ -1,14 +1,19 @@
+import NavbarComponent from "@/components/NavbarComponent";
+import Sidebar from "./_components/Sidebar";
+
 export default function DeveloperDashboardLayout({ children }) {
   return (
-    <div className="flex h-screen">
-      {/* <!-- Sidebar --> */}
-      <div className="w-64 h-full bg-red-400 hidden md:block">
-        <h1>Sidebar</h1>
-        {/* <!-- Sidebar content here --> */}
+    <div className="grid grid-cols-12 w-full h-screen grid-rows-12">
+      <div className="bg-red-500 col-span-2 row-span-12">
+        <Sidebar />
       </div>
-      <div className="flex flex-col flex-grow bg-blue-900">
-        <h1>Navbar</h1>
-        <div className="flex-grow p-6 bg-white">{children}</div>
+      <div className="bg-slate-500 col-span-10 row-span-1">
+        <NavbarComponent />
+      </div>
+      <div className="   col-span-10 row-span-12 rounded-md w-full h-full p-4">
+        <div className="bg-yellow-300 shadow-2xl w-full h-full rounded-md">
+          {children}
+        </div>
       </div>
     </div>
   );
