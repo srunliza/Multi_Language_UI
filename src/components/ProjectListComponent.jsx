a773e1f4955920f742e39675e75f6916473f5502
Sokheng Tey <sokheng.tey03@gmail.com>
2024-06-05 20:52:46 +0700

    project-list

--- src/components/ProjectListComponent.jsx
+++ src/components/ProjectListComponent.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import { projectsData } from "@/obj/projectsData";
+import DeleteProjectModal from "./DeleteProjectModal";
+import ViewMember from "./ViewMember";
 
 const getStatusColor = (status) => {
   switch (status) {
@@ -52,6 +54,7 @@ const ProjectListComponent = () => {
   const [sortOrder, setSortOrder] = useState("asc");
   const [selectedStatus, setSelectedStatus] = useState("All");
   const [sortCriteria, setSortCriteria] = useState("name");
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const modalRef = useRef();
 
@@ -87,10 +90,16 @@ const ProjectListComponent = () => {
     setSelectedProject(null);
   };
 
-  const handleDeleteClick = (projectId) => {
+  const handleDeleteClick = (project) => {
+    setSelectedProject(project);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteConfirm = () => {
     setProjects((prevProjects) =>
-      prevProjects.filter((project) => project.id !== projectId)
+      prevProjects.filter((project) => project.id !== selectedProject.id)
     );
+    setIsDeleteModalOpen(false);
     setSelectedProject(null);
   };
 
@@ -132,7 +141,7 @@ const ProjectListComponent = () => {
   );
 
   return (
-    <div className="p-4 sm:p-6 md:p-8 lg:p-10 flex-1 rounded-xl bg-white mt-6 shadow-lg h-screen overflow-hidden border">
+    <div className="p-4 sm:p-6 md:p-8 lg:p-10 flex-1 rounded-xl w-full bg-white mt-6 shadow-lg h-screen overflow-hidden border">
       <h2 className="text-lg sm:text-xl md:text-2xl font-bold mb-4 text-gray-700">
         My Projects
       </h2>
@@ -248,19 +257,25 @@ const ProjectListComponent = () => {
         {filteredProjects.map((project, index) => (
           <div
             key={index}
-            className="flex justify-between items-center gap-3 bg-slate-250 p-4 shadow rounded-lg border mb-4"
+            className="flex flex-wrap justify-between items-center gap-3 bg-slate-250 p-4 shadow rounded-lg border mb-4"
           >
-            <div className="flex items-center space-x-4 w-[10rem]">
+            <div className="flex flex-col w-[10rem]">
               <div className="text-base font-semibold text-gray-700">
-                {`${project.name.substring(0, 13)}...`}
-                <p className="text-sm font-medium text-green-500">{`${project.owner.substring(
-                  0,
-                  13
-                )}...`}</p>
+                {project.name.length > 15
+                  ? `${project.name.substring(0, 15)}...`
+                  : project.name}
+              </div>
+              <div className="w-[12rem] text-sm text-green-500">
+                {project.owner.length > 15
+                  ? `${project.owner.substring(0, 15)}...`
+                  : project.owner}
+                <span className="text-gray-500 text-xs m-3 ">
+                  {project.role}
+                </span>
               </div>
             </div>
 
-            <div className="flex flex-wrap items-center space-x-6 w-full gap-3 lg:w-auto text-sm mt-4 lg:mt-0">
+            <div className="ml-4 flex flex-wrap items-center gap-3 w-full lg:w-auto text-sm mt-4 lg:mt-0">
               <div className="flex items-center justify-center bg-gray-100 rounded-lg w-16 h-9 text-center text-sm ">
                 <svg
                   className="h-4 w-4 text-black"
@@ -278,7 +293,7 @@ const ProjectListComponent = () => {
                 12
               </div>
 
-              <div className="flex items-center w-[9rem] justify-center bg-red-100 text-red-800 rounded-lg px-5 py-2 text-sm">
+              <div className="flex items-center justify-center w-[10rem] bg-red-100 rounded-lg px-5 py-2 text-sm">
                 <svg
                   className="h-4 w-4 text-red-500 mr-1"
                   fill="none"
@@ -298,8 +313,8 @@ const ProjectListComponent = () => {
                     (project.daysLeft > 1 ? " days left" : " day left")}
               </div>
 
-              <div className="flex bg-black items-center ml-4 gap-7 lg:mt-0 md:mt-3 sm:mt-3">
-                <div className="text-gray-500 text-sm lg:-ml-0 md:-ml-0 sm:-ml-5">
+              <div className="flex flex-wrap items-center sm:ml-5 sm:-mt-1 gap-7 lg:mt-0 md:mt-3">
+                <div className="w-[10rem] md:-ml-4 text-gray-500 text-sm lg:-ml-0 sm:-ml-5">
                   Translation Progress
                 </div>
                 <div
@@ -309,7 +324,7 @@ const ProjectListComponent = () => {
                 >
                   <span>{project.status}</span>
                 </div>
-                <div className="flex-grow rounded-full h-2 w-full lg:w-40 mt-2 lg:mt-0 border bg-slate-200">
+                <div className="flex-grow sm:-ml-4 sm:-mt-4 rounded-full h-2 w-full lg:w-40 mt-2 lg:mt-0 border bg-slate-200">
                   <div
                     className={`h-full ${getStatusBgColor(
                       project.status
@@ -327,33 +342,39 @@ const ProjectListComponent = () => {
               </div>
             </div>
 
-            <div className="flex space-x-6 mt-4 lg:ml-8  lg:mt-0">
-              <div className="flex -space-x-3">
-                <img
-                  src="../assets/images/soklay.png"
-                  alt="User"
-                  className="w-8 h-8 rounded-full border-2 border-white"
-                />
-                <img
-                  src="../assets/images/neath.png"
-                  alt="User"
-                  className="w-8 h-8 rounded-full border-2 border-white"
-                />
-                <img
-                  src="../assets/images/sreyka.png"
-                  alt="User"
-                  className="w-8 h-8 rounded-full border-2 border-white"
-                />
-                <img
-                  src="../assets/images/sreyly.png"
-                  alt="User"
-                  className="w-8 h-8 rounded-full border-2 border-white"
-                />
-                <div className="w-8 h-8 rounded-full bg-gray-300 flex items-center justify-center text-gray-700 text-sm border-2 border-white">
-                  +2
+            <div className="flex mt-4 lg:ml-4  lg:mt-0">
+              <button
+                onClick={handleSeeAll}
+                className="text-gray-600 text-right ml-auto self-end grow"
+              >
+                <div className="flex -space-x-3">
+                  <img
+                    src="../assets/images/soklay.png"
+                    alt="User"
+                    className="w-8 h-8 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="../assets/images/neath.png"
+                    alt="User"
+                    className="w-8 h-8 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="../assets/images/sreyka.png"
+                    alt="User"
+                    className="w-8 h-8 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="../assets/images/sreyly.png"
+                    alt="User"
+                    className="w-8 h-8 rounded-full border-2 border-white"
+                  />
+                  <div className="w-8 h-8 rounded-full bg-gray-300 flex items-center justify-center text-gray-700 text-sm border-2 border-white">
+                    +2
+                  </div>
                 </div>
-              </div>
-              <div className="flex space-x-2">
+              </button>
+
+              <div className="flex space-x-1 ml-5">
                 <button
                   className="focus:outline-none"
                   onClick={() => handleEditClick(project)}
@@ -373,9 +394,10 @@ const ProjectListComponent = () => {
                     <line x1="16" y1="5" x2="19" y2="8" />
                   </svg>
                 </button>
+
                 <button
                   className="focus:outline-none"
-                  onClick={() => handleDeleteClick(project.id)}
+                  onClick={() => handleDeleteClick(project)}
                 >
                   <svg
                     className="h-6 w-6 text-red-500"
@@ -393,10 +415,59 @@ const ProjectListComponent = () => {
                   </svg>
                 </button>
               </div>
+
+              {/* ---View Members Modal--- */}
+              {isViewMemberOpen && (
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-5 flex items-center justify-center z-50">
+                  <div
+                    ref={modalRef}
+                    className="bg-white p-0 rounded-lg shadow-lg w-96"
+                  >
+                    <div className="flex justify-between mt-5 px-4 pt-[6.5rem]">
+                      <div className="flex">
+                        <svg
+                          className="mr-4"
+                          width="26"
+                          height="26"
+                          viewBox="0 0 26 26"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M18.418 21.6663H23.8346V19.4997C23.8346 18.8243 23.6241 18.1656 23.2324 17.6154C22.8408 17.0651 22.2874 16.6505 21.6493 16.4292C21.0111 16.208 20.3199 16.191 19.6717 16.3808C19.0234 16.5705 18.4504 16.9575 18.0323 17.4879M18.418 21.6663H7.58464M18.418 21.6663V19.4997C18.418 18.789 18.2815 18.1098 18.0323 17.4879M18.0323 17.4879C17.63 16.4825 16.9357 15.6208 16.039 15.0137C15.1422 14.4067 14.0842 14.0822 13.0013 14.0822C11.9184 14.0822 10.8604 14.4067 9.96364 15.0137C9.0669 15.6208 8.3726 16.4825 7.9703 17.4879M7.58464 21.6663H2.16797V19.4997C2.16802 18.8243 2.3785 18.1656 2.77015 17.6154C3.1618 17.0651 3.71517 16.6505 4.35332 16.4292C4.99148 16.208 5.68271 16.191 6.33093 16.3808C6.97916 16.5705 7.55217 16.9575 7.9703 17.4879M7.58464 21.6663V19.4997C7.58464 18.789 7.72113 18.1098 7.9703 17.4879M16.2513 7.58301C16.2513 8.44496 15.9089 9.27161 15.2994 9.88111C14.6899 10.4906 13.8633 10.833 13.0013 10.833C12.1393 10.833 11.3127 10.4906 10.7032 9.88111C10.0937 9.27161 9.7513 8.44496 9.7513 7.58301C9.7513 6.72105 10.0937 5.8944 10.7032 5.28491C11.3127 4.67542 12.1393 4.33301 13.0013 4.33301C13.8633 4.33301 14.6899 4.67542 15.2994 5.28491C15.9089 5.8944 16.2513 6.72105 16.2513 7.58301ZM22.7513 10.833C22.7513 11.4076 22.523 11.9587 22.1167 12.3651C21.7104 12.7714 21.1593 12.9997 20.5846 12.9997C20.01 12.9997 19.4589 12.7714 19.0526 12.3651C18.6462 11.9587 18.418 11.4076 18.418 10.833C18.418 10.2584 18.6462 9.70727 19.0526 9.30094C19.4589 8.89461 20.01 8.66634 20.5846 8.66634C21.1593 8.66634 21.7104 8.89461 22.1167 9.30094C22.523 9.70727 22.7513 10.2584 22.7513 10.833ZM7.58464 10.833C7.58464 11.4076 7.35636 11.9587 6.95003 12.3651C6.5437 12.7714 5.9926 12.9997 5.41797 12.9997C4.84333 12.9997 4.29223 12.7714 3.8859 12.3651C3.47958 11.9587 3.2513 11.4076 3.2513 10.833C3.2513 10.2584 3.47958 9.70727 3.8859 9.30094C4.29223 8.89461 4.84333 8.66634 5.41797 8.66634C5.9926 8.66634 6.5437 8.89461 6.95003 9.30094C7.35636 9.70727 7.58464 10.2584 7.58464 10.833Z"
+                            stroke="#475568"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                        <h3 className="text-lg font-semibold mb-4">
+                          40 MEMBERS
+                        </h3>
+                      </div>
+
+                      <svg
+                        className="h-4 w-4 "
+                        onClick={handleModalClose}
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="0px"
+                        y="0px"
+                        width="100"
+                        height="100"
+                        viewBox="0 0 50 50"
+                      >
+                        <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
+                      </svg>
+                    </div>
+                    <ViewMember />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         ))}
       </div>
+
       {isEditing && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
           <div
@@ -441,6 +512,12 @@ const ProjectListComponent = () => {
           </div>
         </div>
       )}
+      {/* Delete Modal */}
+      <DeleteProjectModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={handleDeleteConfirm}
+      />
     </div>
   );
 };
