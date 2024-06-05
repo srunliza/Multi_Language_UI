a744f3611301a15dfebb13316b548f951702bbbe
longsreyly <longsreyly31@gmail.com>
2024-06-05 11:33:26 +0700

    done dashboard att

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,18 +4,14 @@ import SidebarComponent from "@/components/SidebarComponent";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="grid grid-cols-12 w-full h-screen grid-rows-12">
-        <div className="bg-red-500 col-span-2 row-span-12">
-          <SidebarComponent />
-        </div>
-        <div className="col-span-10 row-span-1">
-          <NavbarComponent />
-        </div>
-        <div className="col-span-10 row-span-12 rounded-md w-full h-full p-4">
-          <div className="bg-yellow-300 shadow-2xl w-full h-full rounded-md"></div>
-        </div>
+    <div className="flex h-full ">
+      <aside className="w-[280px] fixed top-0 left-0 h-full">
+        <Sidebar />
+      </aside>
+      <div className="flex-1 ml-[280px]">
+        <NavbarComponent />
+        <main className="p-4">{children}</main>
       </div>
-    </main>
+    </div>
   );
 }
