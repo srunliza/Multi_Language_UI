a15a1cdd04adad0e9fbb716bdee10a1fe5762135
Panha <chhelpanha2@gmail.com>
2024-06-06 13:57:17 +0700

    fixed after merge from sreyly

--- src/app/(dashboard)/project-leader/layout.jsx
+++ src/app/(dashboard)/project-leader/layout.jsx
@@ -8,10 +8,7 @@ export default function DeveloperDashboardLayout({ children }) {
         <Sidebar />
       </aside>
       <div className="flex-1 ml-[280px]">
-      </aside>
-      <div className="flex-1 ml-[280px]">
         <NavbarComponent />
-        <main className="p-4">{children}</main>
         <main className="p-4">{children}</main>
       </div>
     </div>
--- src/app/(dashboard)/translator/layout.jsx
+++ src/app/(dashboard)/translator/layout.jsx
@@ -3,15 +3,12 @@ import Sidebar from "./_components/Sidebar";
 
 export default function DeveloperDashboardLayout({ children }) {
   return (
-    <div className="flex h-full bg-white">
-      <aside className="w-[250px] fixed top-0 left-0 h-full">
+    <div className="flex h-full">
+      <aside className="w-[280px] fixed top-0 left-0 h-full">
         <Sidebar />
       </aside>
       <div className="flex-1 ml-[280px]">
-      </aside>
-      <div className="flex-1 ml-[280px]">
         <NavbarComponent />
-        <main className="p-4">{children}</main>
         <main className="p-4">{children}</main>
       </div>
     </div>
