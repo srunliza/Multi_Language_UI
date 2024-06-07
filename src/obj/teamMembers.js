7d9c730c149fc0a35862787003c118d7a27647c0
Panha <chhelpanha2@gmail.com>
2024-06-07 13:42:45 +0700

    add team member object

---
+++ src/obj/teamMembers.js
@@ -0,0 +1,91 @@
+// teamMembers.js
+export const teamMembers = [
+    {
+        name: "Chhel Chhempanha",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Panha.png",
+        telegram: "https://t.me/chhel_chhempanha",
+        facebook: "https://www.facebook.com/panha.konkhmer.1481",
+        google: "https://mail.google.com/panhach349@gmail.com"
+    },
+    {
+        name: "Mao Piseth",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Piseth.png",
+        telegram: "https://t.me/pisethmao2002",
+        facebook: "https://www.facebook.com/profile.php?id=100068953839160&mibextid=LQQJ4d",
+        google: "@pisethmao2002"
+    },
+    {
+        name: "Tep Thean",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Thean.png",
+        telegram: "https://t.me/thanhxshh",
+        facebook: "https://www.facebook.com/tep.thean.31?mibextid=LQQJ4d",
+        google: "thean.tep.tt@gmail.com"
+    },
+    {
+        name: "Kosal Sreyka",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Soklay.png",
+        telegram: "https://t.me/mengsoklay",
+        facebook: "https://www.facebook.com/mii.lay.184?mibextid=ZbWKwL",
+        google: "mengsoklay2222@gmail.com"
+    },
+    {
+        name: "Kosal Sreyka",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Sreyka.png",
+        telegram: "https://t.me/Karatta22",
+        facebook: "https://www.facebook.com/sreyka.kosal?mibextid=ZbWKwL",
+        google: "kosalsreyka3@gmail.com"
+    },
+    {
+        name: "Long Sreyly",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Sreyly.png",
+        telegram: "https://t.me/long_sreyly",
+        facebook: "https://www.facebook.com/long.menglii.7?mibextid=LQQJ4d",
+        google: "longsreyly85@gmail.com"
+    },
+    {
+        name: "Tan Vibolpheakneat",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Neath.png",
+        telegram: "https://t.me/NeathhZzz",
+        facebook: "https://www.facebook.com/ding.boo.39?mibextid=ZbWKwL",
+        google: "neathtan1402@gmail.com"
+    },
+    {
+        name: "Srun Lisa",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Lisa.png",
+        telegram: "https://t.me/srunlisa",
+        facebook: "https://web.facebook.com/srun.liza/",
+        google: "srunliza@gmail.com"
+    },
+    {
+        name: "Kroem Vandy",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Vandy.png",
+        telegram: "https://t.me/Kroemvandy",
+        facebook: "https://www.facebook.com/profile.php?id=100055021114384&mibextid=ZbWKwL",
+        google: "Kroem123vandy@gmail.com"
+    },
+    {
+        name: "Tey Sok Heng",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Sokheng.png",
+        telegram: "https://t.me/Tey Sok Heng",
+        facebook: "https://www.facebook.com/profile.php?id=100010115617459",
+        google: "sokheng.tey03@gmail.com"
+    },
+    {
+        name: "Ret Sokhim",
+        role: "User interface design and front-end developer",
+        imgSrc: "../Images/Sokhim.png",
+        telegram: "https://t.me/sokhimret",
+        facebook: "https://www.facebook.com/ret.sokhim?mibextid=ZbWKwL",
+        google: "retsokhim21@gmail.com"
+    }
+];
