0d9a434203e48b290e61d9327f545aa734743c47
Sreyka Kosal <108143296+KosalSreyka@users.noreply.github.com>
2024-06-11 22:32:27 +0700

    project leader add delete and edit attachment done

--- src/app/(dashboard)/project-leader/dashboard/preview-page/page.jsx
+++ src/app/(dashboard)/project-leader/dashboard/preview-page/page.jsx
@@ -1,15 +1,10 @@
 "use client"
 import React from "react";
-import { useRouter } from "next/navigation";
 import FeedbackComponent from "@/components/FeedbackComponent";
 import ProjectLeaderPreviewTranslateComponent from "../../_components/ProjectLeaderPreviewComponent";
 
 const PreviewPage = () => {
-  const router = useRouter();
-
-  const handleGoBack = () => {
-    router.push("/project-leader/view-attachment");
-  };
+ 
 
   return (
     <div className="w-[93%] mx-auto mt-5">
@@ -17,14 +12,14 @@ const PreviewPage = () => {
       <div className="grid grid-cols-12 gap-x-10 shadow-sm justify-between mt-5">
         <div className="col-span-8">
           <ProjectLeaderPreviewTranslateComponent />
-          <div className="flex justify-end mt-4">
+          {/* <div className="flex justify-end mt-4">
             <a
               className="cursor-pointer shadow-lg bg-[#7C40FF] rounded-xl py-2 px-3 text-sm text-white"
               onClick={handleGoBack}
             >
               Go Back
             </a>
-          </div>
+          </div> */}
         </div>
         <div className="col-span-4">
           <FeedbackComponent />
